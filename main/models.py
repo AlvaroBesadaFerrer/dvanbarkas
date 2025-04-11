eb70c22489f8500effa0d4dc6f1ccd1d2681967b
Alvaro <besadaferreralvaro@gmail.com>
2025-04-10 21:33:55 -0400

    migration 2

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class ExampleModel(models.Model):
+class ExampleModeleeerrr(models.Model):
     name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='photos/')
