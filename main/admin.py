fc7cabcb00e0dcb0f0d7ae4b36b62837f6fe69a9
Alvaro <besadaferreralvaro@gmail.com>
2025-04-10 21:37:06 -0400

    admin registered model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ExampleModeleeerrr
 
 # Register your models here.
+admin.site.register(ExampleModeleeerrr)  # Register the ExampleModeleeerrr model with the admin site.
